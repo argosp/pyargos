e10d86cb5571be89fdb67e7ec9db9e93bcf1566b
Eden Nitzan <edenn@example.com>
2019-08-07 11:14:43 +0300

    - Adding getTelemetry function to DeviceProxy class

--- thingsboard/tbEntitiesProxy.py
+++ thingsboard/tbEntitiesProxy.py
@@ -1,6 +1,9 @@
 from .tb_api_client.swagger_client import Asset, EntityId, Device,  EntityRelation, EntityId
 from .tb_api_client.swagger_client.rest import ApiException
 
+from pymeteo.datacenter.cassandraBag import CassandraBag
+
+import pandas
 import json
 
 class AbstractProxy(object):
@@ -145,6 +148,15 @@ class DeviceProxy(AbstractProxy):
             return retval.credentials_id
         except ApiException as e:
             return []
+
+    def getTelemetry(self, start_time, end_time, partitions=10, IP='127.0.0.1', db_name='thingsboard', set_name='ts_kv_cf'):
+        cdb = CassandraBag(deviceID=self.id, IP=IP, db_name=db_name, set_name=set_name)
+        bg = cdb.bag(start_time, end_time, partitions)
+        meta = {'ts': int, 'key': str, 'dbl_v': float}
+        bgDataFrame = bg.to_dataframe(meta=meta)
+        df = bgDataFrame.compute().pivot_table(index='ts', columns='key', values='dbl_v')
+        df.index = [pandas.Timestamp.fromtimestamp(x / 1000.0) for x in df.index]
+        return df
 
 
 class AssetProxy(AbstractProxy):
