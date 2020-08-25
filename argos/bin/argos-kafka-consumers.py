d87d110f29ea084f2eae3cc9c3efc67ed271d6b1
eden <edenn@iibr.gov.il>
2020-08-25 12:12:31 +0300

    - Changed bin/argos-kafka-consumers according to last changes

---
+++ argos/bin/argos-kafka-consumers.py
@@ -0,0 +1,41 @@
+from kafka import KafkaConsumer, KafkaProducer
+import argparse
+from argos.kafka import ConsumerProcessor
+import json
+from multiprocessing import Pool
+
+
+def startProcesses(kafkaHost, topic, window, slide, processesDict, expConf):
+    ConsumerProcessor(kafkaHost, topic, window, slide, processesDict, expConf).start()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", dest="config", help="The consumers configuration JSON", required=True)
+    parser.add_argument("--kafkaHost", dest="kafkaHost", default="localhost")
+    parser.add_argument("--expConf", dest="expConf", default='/home/eden/Projects.local/2019/DesertWalls/experimentConfiguration.json')
+    args = parser.parse_args()
+
+    with open(args.config) as configFile:
+        config = json.load(configFile)
+
+    poolNum = 0
+    for topic, topicDict in config.items():
+        for window, windowDict in topicDict.items():
+            for slide, slideDict in windowDict.items():
+                poolNum += len(slideDict)
+    print(poolNum)
+    consumersDict = {}
+
+    with Pool(poolNum) as p:
+        startProcessesInputs = []
+        for topic, topicDict in config.items():
+            # print(topicConfig['topic'])
+            for window, windowDict in topicDict.items():
+                window = None if window=='None' else int(window)
+                for slide, slideDict in windowDict.items():
+                    slide = None if slide=='None' else int(slide)
+                    startProcessesInputs.append((args.kafkaHost, topic, window, slide, slideDict, args.expConf))
+        # print(processesInputs)
+        print('---- ready ----')
+        p.starmap(startProcesses, startProcessesInputs)
