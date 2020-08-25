1e5a0645dee0d7d871c87d264b5474fa992bd10b
eden <edenn@iibr.gov.il>
2020-08-25 12:11:10 +0300

    - Fixed the Processor class and the processes accordingly

--- argos/kafka/consumerProcessor.py
+++ argos/kafka/consumerProcessor.py
@@ -1,27 +1,171 @@
 import pydoc
-
+import pandas
+from . import toPandasDeserializer
+from kafka import KafkaConsumer, KafkaProducer
+from argos import tbHome
+import json
+import paho.mqtt.client as mqtt
+import logging
 
 class ConsumerProcessor(object):
-    _consumer = None
-    _process = None
-    _processArgs = None
+    _kafkaHost = None
+    _topic = None
+    _window = None
+    _slide = None
+    _processes = None
+
+    _windowProcessor = None
+    _kafkaProducer = None
+    _kafkaConsumer = None
+    _tbh = None
+    _tbHost = None
+
+    _clients = None
 
     @property
-    def consumer(self):
-        return self._consumer
+    def clients(self):
+        return self._clients
 
     @property
-    def process(self):
-        return self._process
+    def tbh(self):
+        return self._tbh
 
     @property
-    def processArgs(self):
-        return self._processArgs
+    def tbHost(self):
+        return self._tbHost
 
-    def __init__(self, consumer, process, **processArgs):
-        self._consumer = consumer
-        self._process = process
-        self._processArgs = processArgs
+    @property
+    def kafkaProducer(self):
+        return self._kafkaProducer
+
+    @property
+    def kafkaConsumer(self):
+        return self._kafkaConsumer
+
+    @property
+    def processesDict(self):
+        return self._processesDict
+
+    @property
+    def window(self):
+        return self._window
+
+    @property
+    def slide(self):
+        return self._slide
+
+    def __init__(self, kafkaHost, topic, window, slide, processesDict, expConf):
+        self._kafkaHost = kafkaHost
+        self._topic = topic
+        self._window = window
+        self._slide = slide
+        self._processesDict = processesDict
+        with open(expConf ,'r') as jsonFile:
+            credentialMap = json.load(jsonFile)
+        self._tbh = tbHome(credentialMap["connection"])
+        self._tbHost = credentialMap["connection"]["server"]["ip"]
+
+
+        self._windowProcessor = WindowProcessor(window=window, slide=slide)
+        self._kafkaProducer = KafkaProducer(bootstrap_servers=kafkaHost)
+        self._kafkaConsumer = KafkaConsumer(topic,
+                                            bootstrap_servers=kafkaHost,
+                                            auto_offset_reset='latest',
+                                            enable_auto_commit=True
+                                            # group_id=group_id
+                                            )
+
+        self._clients = dict()
+
+    def on_disconnect(self, client, userdata, rc=0):
+        logging.debug("DisConnected result code " + str(rc))
+        client.loop_stop()
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to broker")
+        else:
+            print("Connection failed")
+
+    def getClient(self, deviceName):
+        if deviceName not in self.clients:
+            client = mqtt.Client("Me_%s" % deviceName)
+            client.on_connect = self.on_connect
+
+            accessToken = self.tbh.deviceHome.createProxy(deviceName).getCredentials()
+            client.username_pw_set(accessToken, password=None)
+            client.on_disconnect = self.on_disconnect
+            client.connect(host=self.tbHost, port=1883)
+            self._clients[deviceName] = client
+            client.loop_start()
+        return self.clients[deviceName]
+
 
     def start(self):
-        pydoc.locate(self._process)(self.consumer, **self.processArgs)
+        for message in self.kafkaConsumer:
+            if self.window is None:
+                data = self._windowProcessor.processMessage(message=message)
+                windowFirstTime = None
+            else:
+                data, windowFirstTime = self._windowProcessor.processMessage(message=message)
+            if data is not None:
+                for process, processArgs in self.processesDict.items():
+                    if windowFirstTime is None:
+                        pydoc.locate(process)(processor=self, data=data, **processArgs)
+                    else:
+                        pydoc.locate(process)(processor=self, data=data, windowFirstTime=windowFirstTime, **processArgs)
+
+
+class WindowProcessor(object):
+    _window = None
+    _slide = None
+    _df = None
+    _lastTime = None
+    _resampled_df = None
+
+    @property
+    def window(self):
+        return self._window
+
+    @property
+    def slide(self):
+        return self._slide
+
+    def __init__(self, window, slide):
+        """
+        :param window: Time window size in seconds if None no window
+        :param slide: Sliding time in seconds if None no slide(and window also None)
+        """
+        self._window = window
+        self._slide = slide
+        self._df = pandas.DataFrame()
+        if self.window is not None:
+            self._n = int(self.window/self.slide)
+        self._lastTime = None
+
+    def processMessage(self, message):
+        if self.window is None:
+            data = self.processMessageWithoutWindow(message=message)
+        else:
+            data = self.processMessageWithWindow(message=message)
+        return data
+
+    def processMessageWithWindow(self, message):
+        self._df = self._df.append(toPandasDeserializer(message.value), sort=True)
+        if self._lastTime is None or (self._lastTime + pandas.Timedelta('%ss' % self.slide) < self._df.tail(1).index[0]):
+            self._resampled_df = self._df.resample('%ss' % self.slide)
+        timeList = list(self._resampled_df.groups.keys())
+        data = None
+        if len(timeList) > self._n:
+            try:
+                if self._lastTime != timeList[0]:
+                    self._lastTime = timeList[0]
+                    data = self._resampled_df.get_group(timeList[0])
+                    self._df = self._df[timeList[1]:]
+            except Exception as exception:
+                print(f'Exception {exception} handled')
+        return data, timeList[0]
+
+    def processMessageWithoutWindow(self, message):
+        print('process message without window')
+        return toPandasDeserializer(message.value)
--- argos/kafka/processes/__init__.py
+++ argos/kafka/processes/__init__.py
@@ -1,12 +1,10 @@
-import pandas
 import paho.mqtt.client as mqtt
-from .. import toPandasDeserializer, pandasDataFrameSerializer, pandasSeriesSerializer
+from .. import pandasDataFrameSerializer, pandasSeriesSerializer
 import logging
 from argos import tbHome
 from hera import meteo
 import json
 from kafka import KafkaProducer
-import time
 
 
 def on_disconnect(client, userdata, rc=0):
@@ -20,128 +18,19 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Connection failed")
 
-def print_mean_T(data):
-    if data is not None:
-        print('Mean T - ', data['T'].mean())
 
-def send_kafka(data, topic, kafkaHost):
-    producer = KafkaProducer(bootstrap_servers=kafkaHost)
-    message = pandasDataFrameSerializer(data)
-    producer.send(topic, message)
-
-def calc_fluctuations(data, topic, kafkaHost='localhost'):
+def calc_fluctuations(processor, data, windowFirstTime, topic):
     trc = meteo.getTurbulenceCalculator(data=data, samplingWindow=None)
     calculatedData = trc.fluctuations().compute()
-    send_kafka(data=data, topic=topic, kafkaHost=kafkaHost)
-    # print(calculatedData)
+    calculatedData.index = [windowFirstTime]
+    message = pandasSeriesSerializer(calculatedData.iloc[0])
+    processor.kafkaProducer.send(topic, message)
 
-def to_thingsboard(data, deviceName, tbHost='localHost', expConf='/home/eden/Projects.local/2019/DesertWalls/experimentConfiguration.json'):
-    with open(expConf, 'r') as credentialOpen:  # with open(args.expConf)
-        credentialMap = json.load(credentialOpen)
-    tbh = tbHome(credentialMap["connection"])
 
-    #deviceName = 'producerTest'
-
-    client = mqtt.Client("Me_%s" % deviceName)
-    accessToken = tbh.deviceHome.createProxy(deviceName).getCredentials()
-    client.username_pw_set(accessToken, password=None)
-    client.on_disconnect = on_disconnect
-    client.connect(tbHost)
-    client.loop_start()
+def to_thingsboard(processor, data, deviceName):
+    client = processor.getClient(deviceName=deviceName)
 
     data.index = [x.tz_localize('israel') for x in data.index]
     client.publish('v1/devices/me/telemetry', pandasDataFrameSerializer(data))
-
-
-
-def print_fluctuations2(data, deviceName, tbHost='localHost', expConf='/home/eden/Projects.local/2019/DesertWalls/experimentConfiguration.json'):
-    with open(expConf, 'r') as credentialOpen:  # with open(args.expConf)
-        credentialMap = json.load(credentialOpen)
-    tbh = tbHome(credentialMap["connection"])
-
-    #deviceName = 'producerTest'
-
-    client = mqtt.Client("Me_%s" % deviceName)
-    accessToken = tbh.deviceHome.createProxy(deviceName).getCredentials()
-    client.username_pw_set(accessToken, password=None)
-    client.on_disconnect = on_disconnect
-    client.connect(tbHost)
-    client.loop_start()
-
-    trc = meteo.getTurbulenceCalculator(data=data, samplingWindow=None)
-    calculatedData = trc.fluctuations().compute()
-    calculatedData.index = [x.tz_localize('israel') for x in calculatedData.index]
-    print(calculatedData)
-    client.publish('v1/devices/me/telemetry', pandasDataFrameSerializer(calculatedData))
-
-def print_sliding_window_old(consumer, window, slide):
-    print(window, '---', slide)
-
-    df = pandas.DataFrame()
-    lastTime = None
-    n = int(window/slide)
-
-    for message in consumer:
-        df = df.append(toPandasDeserializer(message.value), sort=True)
-        if lastTime is None:
-            lastTime = df.index[0]
-            resampled_df = df.resample('%ss' % slide)
-        elif lastTime + pandas.Timedelta('%ss' % slide) < df.tail(1).index[0]:
-            resampled_df = df.resample('%ss' % slide)
-        timeList = list(resampled_df.groups.keys())
-        if len(timeList) > n:
-            try:
-                if lastTime != timeList[0]:
-                    lastTime = timeList[0]
-                    print('Mean T - ', resampled_df.get_group(timeList[0]).mean()['T'])
-                    # import pdb
-                    # pdb.set_trace()
-                    df = df[timeList[1]:]
-            except Exception as exception:
-                print(f'Exception {exception} handled')
-
-
-def publish_sliding_window_fluctuations_old(consumer, window, slide, deviceName, tbHost='localhost', expConf='/home/eden/Projects.local/2019/DesertWalls/experimentConfiguration.json'):
-    print(window, '---', slide, '---', 'fluctuations')
-
-    with open(expConf, 'r') as credentialOpen:  # with open(args.expConf)
-        credentialMap = json.load(credentialOpen)
-    tbh = tbHome(credentialMap["connection"])
-
-    #deviceName = 'producerTest'
-
-    client = mqtt.Client("Me_%s" % deviceName)
-    accessToken = tbh.deviceHome.createProxy(deviceName).getCredentials()
-    client.username_pw_set(accessToken, password=None)
-    client.on_disconnect = on_disconnect
-    client.connect(tbHost)
-    client.loop_start()
-
-    df = pandas.DataFrame()
-    lastTime = None
-    n = int(window / slide)
-
-    for message in consumer:
-        df = df.append(toPandasDeserializer(message.value), sort=True)
-        if lastTime is None:
-            lastTime = df.index[0]
-            resampled_df = df.resample('%ss' % slide)
-        elif lastTime + pandas.Timedelta('%ss' % slide) < df.tail(1).index[0]:
-            resampled_df = df.resample('%ss' % slide)
-        timeList = list(resampled_df.groups.keys())
-        if len(timeList) > n:
-            try:
-                if lastTime != timeList[0]:
-                    lastTime = timeList[0]
-                    data = resampled_df.get_group(timeList[0])
-                    data.index = [lastTime if i==0 else data.index[i] for i in range(len(data.index))]
-                    trc = meteo.getTurbulenceCalculator(data=data, samplingWindow=None)
-                    calculatedData = trc.fluctuations().compute()
-                    calculatedData.index = [x.tz_localize('israel') for x in calculatedData.index]
-                    print(calculatedData)
-                    client.publish('v1/devices/me/telemetry', pandasDataFrameSerializer(calculatedData))
-                    # import pdb
-                    # pdb.set_trace()
-                    df = df[timeList[1]:]
-            except Exception as exception:
-                print(f'Exception {exception} handled')
+    client.loop_stop()
+    client.disconnect()
