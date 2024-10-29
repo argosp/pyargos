cc93c64e0c3058a45bf368de8426392282b52d9d
Eran Geva <eran.geva@gmail.com>
2024-10-29 17:01:28 +0100

    nicer prints on runner

--- argos/experimentSetup/runner.py
+++ argos/experimentSetup/runner.py
@@ -2,12 +2,20 @@ import json
 import logging
 from argos.experimentSetup.dataObjectsFactory import fileExperimentFactory
 
+# choose experiment
 # experimentDirectory = 'argos/experimentSetup/example_exp/exp_simple'
 experimentDirectory = 'argos/experimentSetup/example_exp/exp_groups'
+
+# analyze it
 fact = fileExperimentFactory(experimentDirectory)
 fact.logger.setLevel('DEBUG')
 fact.logger.addHandler(logging.StreamHandler())
 exp = fact.getExperiment()
+
+# printing
 for e in exp.__dict__:
-    print(e, exp.__dict__[e])
-    print()
+    try:
+        s = json.dumps(exp.__dict__[e], indent=2)     
+    except:
+        s = str(exp.__dict__[e])
+    print(e, ':\t', s, '\n')
