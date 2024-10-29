43382a0f95d8d6930de03797a0ec96bcf818cc7c
Eran Geva <eran.geva@gmail.com>
2024-10-29 16:35:22 +0100

    adding example exp with groups

--- argos/experimentSetup/runner.py
+++ argos/experimentSetup/runner.py
@@ -2,7 +2,8 @@ import json
 import logging
 from argos.experimentSetup.dataObjectsFactory import fileExperimentFactory
 
-experimentDirectory = 'argos/experimentSetup/example_exp/'
+# experimentDirectory = 'argos/experimentSetup/example_exp/exp_simple'
+experimentDirectory = 'argos/experimentSetup/example_exp/exp_groups'
 fact = fileExperimentFactory(experimentDirectory)
 fact.logger.setLevel('DEBUG')
 fact.logger.addHandler(logging.StreamHandler())
