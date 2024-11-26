c58398389d0e22bb1bb7bd933e3d6a02651670ce
Eran Geva <eran.geva@gmail.com>
2024-11-26 11:31:51 +0100

    unify_all_logs_debug func

--- argos/experimentSetup/runner.py
+++ argos/experimentSetup/runner.py
@@ -1,15 +1,21 @@
 import json
 import logging
+import sys
 from argos.experimentSetup.dataObjectsFactory import fileExperimentFactory
+from argos.utils.logging.helpers import unify_all_logs_debug
+from ..utils.logging import get_logger as argos_get_logger
 
 # choose experiment
 # experimentDirectory = 'argos/experimentSetup/example_exp/exp_simple'
 experimentDirectory = 'argos/experimentSetup/example_exp/exp_groups'
+unify_all_logs_debug()
+logger = argos_get_logger("")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+logger.info(f"Hello runner!")
 
 # analyze it
 fact = fileExperimentFactory(experimentDirectory)
-fact.logger.setLevel('DEBUG')
-fact.logger.addHandler(logging.StreamHandler())
 exp = fact.getExperiment()
 
 # printing
